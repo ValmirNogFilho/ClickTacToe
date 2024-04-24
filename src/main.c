adf0cca0ef3c4a44765186912a73015a35b33414
Valmir Filho <119276059+ValmirNogFilho@users.noreply.github.com>
2024-04-24 20:40:18 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -14,15 +14,15 @@ int main() {
     char jogador = 'X';
     int jogadas = 0;
     int valor;
-    int t;
-    ssize_t n;
-    int eUm, eDois;
-    char vencedor;
-    struct input_event ev;
+    int partida_despausada; // verdadeiro ou falso
+    ssize_t n; // número de bytes retornado pelo arquivo do driver do mouse
+    int nova_atualizacao_a_direita, nova_atualizacao_a_esquerda; // detecta atualização de movimento após despausar a partida
+    char vencedor; // caractere do jogador que venceu 
+    struct input_event ev; // struct que recebe as informações do evento de locomoção do mouse
     
-    const char *dev = "/dev/input/event0"; 
+    const char *dev = "/dev/input/event0";  // endereço do driver utilizado
     int fd = open(dev, O_RDONLY);
-    if (fd == -1) {
+    if (fd == -1) { // falha na abertura do arquivo
         perror("Cannot open input device");
         return 1;
     }
@@ -32,36 +32,40 @@ int main() {
     exibir_tabuleiro();
     valor = 0;
     int *dados_sw = &valor;
-    t = 0;
-    SW_open();
-    SW_read(dados_sw);
+    partida_despausada = 0;
+    SW_open(); // abertura do driver das chaves HH
+    SW_read(dados_sw); // leitura inicial para descobrir se o jogo começou pausado
     // Loop infinito para receber as coordenadas
     while (1) {
     SW_read(dados_sw);
-      
+
+      // leitura do driver do mouse
       n = read(fd, &ev, sizeof(ev));
 
       if(*dados_sw==1){
-      if (n == (ssize_t)-1) {
+      if (n == (ssize_t)-1) { // se não foi lido nenhum byte no driver do mouse
           perror("Error reading");
           break;
-      } else if (n != sizeof(ev)) {
+      } else if (n != sizeof(ev)) { // espera se a leitura de 16 bytes do driver do mouse. Qualquer número de bytes fora do esperado levanta uma exceção.
           fprintf(stderr, "Error: read %d bytes, expecting %d\n", n, sizeof(ev));
           break;
       }
 
-      if(t==1){
+      if(partida_despausada==1){
         system("clear");
-  t=0;
-  ev.value=0; 
-}
+        partida_despausada=0; // toggle do valor simbolizando retorno da partida
+        ev.value=0; 
+      }
 
-      if (ev.type == EV_REL && ev.code == REL_X) {
-        x =  ev.value;}
-      if (ev.type == EV_REL && ev.code == REL_Y) {
-        y = ev.value;}
+      if (ev.type == EV_REL && ev.code == REL_X) { // se a locomoção detectada no mouse é horizontal
+        x =  ev.value;
+      }
+      if (ev.type == EV_REL && ev.code == REL_Y) {  // se a locomoção detectada no mouse é vertical
+        y = ev.value;
+      }
 
-      if(x > 50){
+      // detecção de bordas dos deslocamentos X e Y
+      if(x > 50){ 
         x = 50;
       }else if (x < -50){
         x = -50;
@@ -73,7 +77,7 @@ int main() {
         y = -50;
       }
 
-        // Acumula as coordenadas
+      // Acumula as coordenadas
       if(xsoma <= MAX && xsoma >= MIN){
         xsoma += x;
         if(xsoma > MAX) {
@@ -112,45 +116,49 @@ int main() {
       } else if (xsoma > MAX_QUADRANTE_2 && xsoma <= MAX_QUADRANTE_3 && ysoma > MAX_QUADRANTE_2 && ysoma <= MAX_QUADRANTE_3) {
         escolha = 9;
       }
-      eUm = escolha+1;
-      eDois = escolha-1;
-      if(escolha == eUm || escolha ==eDois || ev.value ==1){
-        system("clear");
+      nova_atualizacao_a_direita = escolha+1;
+      nova_atualizacao_a_esquerda = escolha-1;
+      if(escolha == nova_atualizacao_a_direita || escolha == nova_atualizacao_a_esquerda || ev.value ==1){ // se foi detectada novo evento de locomoção do mouse ou clique
+        system("clear"); // atualização do tabuleiro impresso na tela
         exibir_tabuleiro();
         printf("Quadrante %d\n", escolha);
       }
+
+      //se foi detectado um clique com o botão esquerdo do mouse
       if(ev.type == EV_KEY && ev.code == BTN_LEFT && ev.value == 1){
-        int linha = (escolha - 1) / 3;
-        int coluna = (escolha - 1) % 3;
-    system("clear");
-        // Verifica se a posição escolhida já está ocupada
-        if (posicao_ocupada(linha, coluna)) {
-            printf("Posição já ocupada. Escolha outra posição.\n");
-            continue;
-        }
-
-        // Marca a posição escolhida com o símbolo do jogador atual
-        preencher_no_tabuleiro(linha, coluna, jogador);
-        exibir_tabuleiro();
-        printf("Quadrante %d\n", escolha);
-
-        // Verifica se houve um vencedor
-        vencedor = verificar_vencedor();
-        if (vencedor != ' ') {
-            printf("Parabéns! O jogador %c venceu!\n", vencedor);
-            break;
-        }
-
-        // Verifica se o jogo terminou em empate
-        jogadas++;
-        if (jogadas == 9) {
-            printf("O jogo terminou em empate!\n");
-            break;
-        }
-
-        // Alternar jogador
-        jogador = (jogador == 'X') ? 'O' : 'X';
-      }}
+            // cálculo do quadrante adequado para o tabuleiro
+            int linha = (escolha - 1) / 3; 
+            int coluna = (escolha - 1) % 3;
+            system("clear");
+            // Verifica se a posição escolhida já está ocupada
+            if (posicao_ocupada(linha, coluna)) {
+                printf("Posição já ocupada. Escolha outra posição.\n");
+                continue;
+            }
+    
+            // Marca a posição escolhida com o símbolo do jogador atual
+            preencher_no_tabuleiro(linha, coluna, jogador);
+            exibir_tabuleiro();
+            printf("Quadrante %d\n", escolha);
+    
+            // Verifica se houve um vencedor
+            vencedor = verificar_vencedor();
+            if (vencedor != ' ') {
+                printf("Parabéns! O jogador %c venceu!\n", vencedor);
+                break;
+            }
+    
+            // Verifica se o jogo terminou em empate
+            jogadas++; // contabiliza o número de rodadas. 9 rodadas sem um vencedor implica em um empate
+            if (jogadas == 9) {
+                printf("O jogo terminou em empate!\n");
+                break;
+            }
+    
+            // Alternar jogador
+            jogador = (jogador == 'X') ? 'O' : 'X';
+      }
+      }
 
 }
   SW_close();
